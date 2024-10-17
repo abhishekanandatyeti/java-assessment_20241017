baf18fdc435be8ddd1377a4a4886fb3c5f303b38
shoheb1012 <shoheb.pathan@atyeti.com>
2024-10-17 11:41:03 +0000

    Add test

--- myapp/src/main/java/com/atyeti/myapp/DebugExample.java
+++ myapp/src/main/java/com/atyeti/myapp/DebugExample.java
@@ -7,8 +7,15 @@ public class DebugExample {
             System.out.println(numbers[i]);
         }
     }
+    //Index 5 out of bounds for length 5
+    //in the above program we are starting loop with 1 and it equals with length
+
 
     public void printNumbersSafely() {
+      
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
 
-    }
+    }   // its correct
 }
--- myapp/src/main/java/com/atyeti/myapp/Vehicle.java
+++ myapp/src/main/java/com/atyeti/myapp/Vehicle.java
@@ -2,5 +2,27 @@ package com.atyeti.myapp;
 
 public class Vehicle {
 
+    private String VehicleType;
+    private String VehicleColor;
+    
+    public Vehicle( String VehicleType,String VehicleColor)
+    {
+        this.VehicleType=VehicleType;
+        this.VehicleColor=VehicleColor;
+
+    }
+    public String getVehicleType()
+    {
+        return VehicleType;
+    }
+    public String getVehicleColor()
+    {
+        return VehicleColor;
+    }
+    public String toString()
+    {
+        return "Vehicle Type "+VehicleType+"vehicle COlor "+VehicleColor;
+    }
+
 }
 
